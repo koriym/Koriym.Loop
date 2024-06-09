8be3c31863215d5f16162024c7acf59455b2182c
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-09 11:02:11 +0900

    Implement LoopGenInterface in LoopGen class
    The class LoopGen has been refactored to implement the LoopGenInterface, encapsulating the loop generating logic into an interface. This change makes the LoopGen class cleaner and easier to understand, as it now adheres to the defined contract of the interface. All methods and parameters have been updated accordingly.

---
+++ src/LoopGenInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Koriym\Loop;
+
+use Generator;
+use Iterator;
+
+/**
+ * @template T of object
+ */
+interface LoopGenInterface
+{
+    /**
+     * This function generates a loop for the given elements. It takes an iterator or an array of elements,
+     * a class-string entity, an optional array of extra parameters, and an optional factory callable.
+     * It returns a generator that yields a new Loop object for each element.
+     *
+     * @param Iterator<array-key, array<string, mixed>>|array<array<string, mixed>> $elements
+     * @param class-string<T>                                                       $entity
+     * @param array<string, mixed>                                                  $extraParams
+     * @param callable(class-string, mixed...):T                                    $factory
+     *
+     * @return Generator<Loop, T, mixed, void>
+     *
+     * @psalm-suppress ImplementedReturnTypeMismatch
+     */
+    public function __invoke($elements, string $entity, array $extraParams = [], ?callable $factory = null): Generator;
+}
